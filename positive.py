f538f5d98a716ea771da39fb9770ddf79764bb67
saitharunnch <80032061+saitharunnch@users.noreply.github.com>
2021-03-06 16:28:54 +0530

    created positive.py
    prints positive numbers

---
+++ positive.py
@@ -0,0 +1,10 @@
+list1=[12,13,-10,44,-39,90]
+for num in list1:
+    if num>=0:
+        print( num,end =" ")
+list2=[14,15,-20,78,-13]
+for num in list2:
+    if num>0:
+        print(num,end =" ")
+    
+      
